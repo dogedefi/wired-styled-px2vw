5a276167b51e1311bd3c7091dbed85eebdc4300b
dogedefi <myz.defi@gmail.com>
2022-05-23 20:26:51 +0800

    enhance: custom mobile width

--- index.js
+++ index.js
@@ -1,55 +1,63 @@
-import styled from 'styled-components';
-import throttle from 'lodash.throttle';
+/* eslint-disable no-debugger */
+import styled from "styled-components";
+import throttle from "lodash.throttle";
 
 const pxRe = /-?\d*[.\d]*px/g;
 const base64Re = /^data:\w+\/[a-zA-Z+\-.]+;base64,/i;
 
 let isMobile = false;
-const MOBILE_WIDTH = 768;
- const getWindowScreen = throttle(() => {
-     isMobile =
-         window.innerWidth <= MOBILE_WIDTH ||
-         /mobile|ios|android/gi.test(navigator.userAgent);
+const MOBILE_WIDTH = process.env.MOBILE_WIDTH ?? 768;
+const checkScreen = () => {
+    isMobile =
+        window.innerWidth <= MOBILE_WIDTH ||
+        /mobile|ios|android/gi.test(navigator.userAgent);
 
-     // chrome
-     if (
-         /chrome/gi.test(navigator.userAgent) &&
-         window.innerWidth > MOBILE_WIDTH
-     ) {
-         isMobile = false;
-     } else {
-         isMobile = true;
-     }
- }, 100);
+    // chrome
+    if (
+        /chrome/gi.test(navigator.userAgent) &&
+        window.innerWidth > MOBILE_WIDTH
+    ) {
+        isMobile = false;
+    } else {
+        isMobile = true;
+    }
+};
+const getWindowScreen = throttle(checkScreen, 100);
 window.addEventListener("resize", getWindowScreen, false);
+checkScreen();
 
-const px2vw = px => Number(px) ? `${Math.round(Number(px) / (isMobile ? 7.5 : 14.4) * 100000) / 100000}vw` : 0;
+const px2vw = (px) =>
+    Number(px)
+        ? isMobile
+            ? `${Math.round((Number(px) / 7.5) * 100000) / 100000}vw`
+            : `${px}px`
+        : 0;
 
-const convertStringPx2vw = style => {
+const convertStringPx2vw = (style) => {
     if (!style) return style;
 
     if (
-        !base64Re.test(style)   // 非base64字符串
-        && pxRe.test(style)     // 包含px单位
+        !base64Re.test(style) && // 非base64字符串
+        pxRe.test(style) // 包含px单位
     ) {
-
-        return style.replace(pxRe, value => px2vw(value.replace('px', '')))
+        return style.replace(pxRe, (value) => px2vw(value.replace("px", "")));
     }
 
     return style;
-}
+};
 
-const isKeyframes = interpolation => Object.prototype.toString.call(interpolation) === '[object Object]'
-    && interpolation.constructor.name === 'Keyframes';
+const isKeyframes = (interpolation) =>
+    Object.prototype.toString.call(interpolation) === "[object Object]" &&
+    interpolation.constructor.name === "Keyframes";
 
-const convertKeyframesPx2vw = keyframes => {
+const convertKeyframesPx2vw = (keyframes) => {
     keyframes.stringifyArgs = keyframes.stringifyArgs.map(convertStringPx2vw);
 
     return keyframes;
 };
 
-const convertInterpolationPx2vw = interpolation => {
-    if (typeof interpolation === 'string') {
+const convertInterpolationPx2vw = (interpolation) => {
+    if (typeof interpolation === "string") {
         return convertStringPx2vw(interpolation);
     }
 
@@ -61,38 +69,43 @@ const convertInterpolationPx2vw = interpolation => {
         return interpolation.map(convertInterpolationPx2vw);
     }
 
-    if (typeof interpolation === 'function') {
-        return props => convertInterpolationPx2vw(interpolation(props))
+    if (typeof interpolation === "function") {
+        return (props) => convertInterpolationPx2vw(interpolation(props));
     }
 
     return interpolation;
-
 };
 
-const withCss = styled => {
+const withCss = (styled) => {
     const interleave = (strings, ...interpolations) => {
-
         strings = strings.map(convertStringPx2vw);
 
         interpolations = interpolations.map(convertInterpolationPx2vw);
 
         return styled(strings, ...interpolations);
-    }
+    };
 
-    Object.keys(styled).forEach(prop => interleave[prop] = withTemplateFunc(styled[prop]));
+    Object.keys(styled).forEach(
+        (prop) => (interleave[prop] = withTemplateFunc(styled[prop]))
+    );
 
     return interleave;
 };
 
-const withTemplateFunc = styled => (...props) => withCss(styled(...props));
+const withTemplateFunc =
+    (styled) =>
+    (...props) =>
+        withCss(styled(...props));
 
 const styledPx2vw = ((styled) => {
     const obj = withTemplateFunc(styled);
 
-    Object.keys(styled).forEach(key => {
+    Object.keys(styled).forEach((key) => {
         obj[key] = withCss(styled[key]);
 
-        Object.keys(styled[key]).forEach(prop => obj[key][prop] = withTemplateFunc(styled[key][prop]));
+        Object.keys(styled[key]).forEach(
+            (prop) => (obj[key][prop] = withTemplateFunc(styled[key][prop]))
+        );
     });
 
     return obj;
@@ -100,4 +113,4 @@ const styledPx2vw = ((styled) => {
 
 export default styledPx2vw;
 export { px2vw };
-export * from 'styled-components';
+export * from "styled-components";
