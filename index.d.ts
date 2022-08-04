c49dca3e66a8aed5134c40973036f8b56daab9a6
Benhaben <web3monkey@proton.me>
2022-08-04 14:39:27 +0800

    fix: declare files

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 import { StyledInterface } from 'styled-components'
 
-declare const styledPx2vw : StyledInterface
+declare const styledPx2vw: StyledInterface
 
 export default styledPx2vw
 
-export const px2vw: (px: string) => (string | 0);
+export const px2vw: (px: string) => string | 0
+export const checkIfMobile: (boundary: number) => boolean
 
 export * from 'styled-components'
